47120a7548b3b576c1db362c7c8e0bc428a2c860
Riley <riley.childs@yahoo.com>
2022-08-29 19:01:15 -0400

    Got Job component set up

---
+++ jobListings/src/Components/Job.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let title;
+    export let imgSrc;
+    export let postDate;
+    export let commitment;
+    export let location
+</script>
+
+
+
+<section class="job">
+    <div class="job__photo">
+        <img src="{imgSrc}" alt="{title}">
+    </div>
+    <h4 class="job__title">{title}</h4>
+    <span class="job__details">
+        <p>{postDate} &#183;</p>
+        <p>{commitment} &#183;</p>
+        <p>{location} &#183;</p>
+    </span>
+</section>
+
+<style>
+.job{
+    border: solid;
+    width: 75%;
+    margin: 2rem;
+}
+</style>
