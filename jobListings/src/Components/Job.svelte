84a76eb6f8653fcd6877b5ccc9474e27b0c4dcd5
Riley <riley.childs@yahoo.com>
2022-08-30 13:44:25 -0400

    Added more HTML markup to Job.svelte

--- jobListings/src/Components/Job.svelte
+++ jobListings/src/Components/Job.svelte
@@ -13,34 +13,37 @@
   export let tools;
 </script>
 
+<style>
+    .job {
+      border: solid;
+      width: 80%;
+      margin-top: 0.5rem;
+      margin-bottom: 0.5rem;
+      padding: 1rem;
+    }
+  </style>
+  
+
 <section class="job">
   <div class="job__photo">
     <img src={logo} alt={company} />
   </div>
-  <span class="job__newFeatured">
-    <p>{newPost}</p>
-    <p>{featured}</p>
+  <span class="job__company">
+    <p class="company">{company}</p>
+    <p class="new">{newPost}</p>
+    <p class="featured">{featured}</p>
   </span>
   <h4 class="job__title">{position}</h4>
   <span class="job__details--commitment">
-    <p>{postedAt} &#183;</p>
-    <p>{contract} &#183;</p>
-    <p>{location} &#183;</p>
+    <p class="posted-at">{postedAt} &#183;</p>
+    <p class="contract">{contract} &#183;</p>
+    <p class="location">{location} &#183;</p>
   </span>
   <span class="job__details--stack">
-    <p>{role}</p>
-    <p>{level}</p>
-    <p>{languages}</p>
-    <p>{tools}</p>
+    <p class="role">{role}</p>
+    <p class="level">{level}</p>
+    <p class="langiages">{languages}</p>
+    <p class="tools">{tools}</p>
   </span>
 </section>
 
-<style>
-  .job {
-    border: solid;
-    width: 80%;
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-    padding: 1rem;
-  }
-</style>
