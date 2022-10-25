c13a828fdd03c795bee5eb8b109e9ae483b1f3b0
Riley <riley.childs@yahoo.com>
2022-10-25 18:36:15 -0400

    Fixed bud in MONGODB URI variable

--- jobListings/mongoose.js
+++ jobListings/mongoose.js
@@ -4,7 +4,6 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const mongoose = require("mongoose");
 const localDB = 'mongodb://127.0.0.1:27017/job-filters'
 const db = mongoose.connection
-console.log(process.env.MONGODB_URI)
 // Set up MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
