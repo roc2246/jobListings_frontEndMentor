4dac99053236cf754727e12957d417ec4a5f67c5
Riley <riley.childs@yahoo.com>
2022-10-25 12:45:23 -0400

    Connected app to live database

--- jobListings/mongoose.js
+++ jobListings/mongoose.js
@@ -4,11 +4,10 @@ const mongoose = require("mongoose");
 const localDB = 'mongodb://127.0.0.1:27017/job-filters'
 const dbName = "comments"
 // const MONGODB_URI = `mongodb+srv://childswebdev:${password}@comment-section-cluster.mn0pidu.mongodb.net/${dbName}?retryWrites=true&w=majority`
-require("dotenv").config(); 
 const db = mongoose.connection
 
 // Set up MongoDB
-mongoose.connect(localDB, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
   })
 
